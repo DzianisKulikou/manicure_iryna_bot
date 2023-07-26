c7ec1ec63997c3b4ff25da6640d06351b9fc817e
Dzianis Kulikou <d-rival@mail.ru>
2023-07-26 15:12:42 +0200

    Выносим надписи кнопок в lexicon_ru.py

--- keyboards/keyboard_utils.py
+++ keyboards/keyboard_utils.py
@@ -1,13 +1,14 @@
 from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup,
                            ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup)
+from lexicon.lexicon_ru import lexicon_button
 
 
 # Создаем объекты кнопок 'Главное Меню'
-button_1: KeyboardButton = KeyboardButton(text='Мой адрес')
-button_2: KeyboardButton = KeyboardButton(text='Мой контактный телефон')
-button_3: KeyboardButton = KeyboardButton(text='Показать фото маникюра')
-button_4: KeyboardButton = KeyboardButton(text='Показать случайное фото маникюра')
-button_5: KeyboardButton = KeyboardButton(text='Написать Ирине в Telegram')
+button_1: KeyboardButton = KeyboardButton(text=lexicon_button['button_1'])
+button_2: KeyboardButton = KeyboardButton(text=lexicon_button['button_2'])
+button_3: KeyboardButton = KeyboardButton(text=lexicon_button['button_3'])
+button_4: KeyboardButton = KeyboardButton(text=lexicon_button['button_4'])
+button_5: KeyboardButton = KeyboardButton(text=lexicon_button['button_5'])
 
 # Создаем объект клавиатуры, добавляя в него кнопки 'Главное Меню'
 keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_1, button_2],
@@ -17,7 +18,7 @@ keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_1, button_
 
 # Создаем объекты инлайн-кнопок
 url_button_1: InlineKeyboardButton = InlineKeyboardButton(
-                                    text='Написать Ирине в Telegram',
+                                    text=lexicon_button['url_button_1'],
                                     url='https://t.me/Ir1shka24')
 
 # Создаем объект инлайн-клавиатуры
