53302362dd1a849809fe4e9f614bc34e4943cd15
Dzianis Kulikou <d-rival@mail.ru>
2023-07-27 16:46:58 +0200

    Создаём директорию disinfection и копируем нужные фото. В ДБ добавляем словарь с перечнем фотографий.

--- database/database_photo.py
+++ database/database_photo.py
@@ -8,3 +8,11 @@ photo_nails: dict[int, str] = {1: 'photo/nails/101.jpg', 2: 'photo/nails/102.jpg
                                7: 'photo/nails/107.jpg', 8: 'photo/nails/108.jpg', 9: 'photo/nails/109.jpg',
                                10: 'photo/nails/110.jpg', 11: 'photo/nails/111.jpg', 12: 'photo/nails/112.jpg'
                                }
+
+photo_disinfection: dict[int, str] = {1: 'photo/disinfection/201.jpg', 2: 'photo/disinfection/202.jpg',
+                                      3: 'photo/disinfection/203.jpg', 4: 'photo/disinfection/204.jpg',
+                                      5: 'photo/disinfection/205.jpg', 6: 'photo/disinfection/206.jpg',
+                                      7: 'photo/disinfection/207.jpg', 8: 'photo/disinfection/208.jpg',
+                                      9: 'photo/disinfection/209.jpg', 10: 'photo/disinfection/210.jpg',
+                                      11: 'photo/disinfection/211.jpg', 12: 'photo/disinfection/212.jpg'
+                                      }
