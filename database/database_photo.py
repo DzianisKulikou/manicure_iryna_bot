e615a97854bee83c5b376c4b111512aba8e0407c
Dzianis Kulikou <d-rival@mail.ru>
2023-08-01 20:28:57 +0200

    Добавляем фотографии в photo_nails.

--- database/database_photo.py
+++ database/database_photo.py
@@ -6,7 +6,8 @@ photo_map = 'photo/maps/1.jpg'
 photo_nails: dict[int, str] = {1: 'photo/nails/101.jpg', 2: 'photo/nails/102.jpg', 3: 'photo/nails/103.jpg',
                                4: 'photo/nails/104.jpg', 5: 'photo/nails/105.jpg', 6: 'photo/nails/106.jpg',
                                7: 'photo/nails/107.jpg', 8: 'photo/nails/108.jpg', 9: 'photo/nails/109.jpg',
-                               10: 'photo/nails/110.jpg', 11: 'photo/nails/111.jpg', 12: 'photo/nails/112.jpg'
+                               10: 'photo/nails/110.jpg', 11: 'photo/nails/111.jpg', 12: 'photo/nails/112.jpg',
+                               13: 'photo/nails/113.jpg', 14: 'photo/nails/114.jpg', 15: 'photo/nails/115.jpg'
                                }
 
 photo_disinfection: dict[int, str] = {1: 'photo/disinfection/201.jpg', 2: 'photo/disinfection/202.jpg',
