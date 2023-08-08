07ce3a0cdc573ce0d30794fe2861173cac7fc85f
Dzianis Kulikou <d-rival@mail.ru>
2023-08-08 12:51:30 +0200

    Создаём файл lexicon_en.py переводим основные фразы на английский язык.

---
+++ lexicon/lexicon_en.py
@@ -0,0 +1,80 @@
+lexicon_dict_en: dict[str, str] = {
+    'start': "<b>Hi!\nI'm Iryna Kulikova's bot!</b>\n"
+              "I can tell you all the information about her activities as a manicure master!\n"
+              "<span class='tg-spoiler'>_____________________________________</span>",
+    '/help': "Here you can find out everything about Iryna Kulikova's work as a manicure Master!\n"
+             'If you have any problems with the bot (something does not open), then the bot has been rebooted.'
+             'To make everything work correctly, just restart it by writing the command in the chat: '
+             '"<b>/start</b>". Or click on the command: "<b>/start</b>" - '
+             'in <b>Menu</b> Bot!',
+    'unknown_message': 'I do not know what to answer you...'
+                       '\n(Use the built-in keyboard at the bottom👇)',
+    'menu': 'What would you like to know?\n(Use the built-in keyboard at the bottom👇)',
+    'adres': 'Warszawa 02-582, ul. Wiktorska 83/87\nOld Mokotov district\n300 meters from the metro station Racławicka',
+    'phone': '+48-451-117-579 Iryna',
+    'nails': 'These are photos of my works:',
+    'me': 'If you want to write to me in Telegram, click on the button under the message and you will open a chat '
+          'with me!',
+    'start_manicure': 'In this section you can get acquainted with my works, processes, materials and tools!'
+                      '\n(Use the built-in keyboard at the bottom👇)',
+    'kontakt': 'In this section you can find my address and contact details.'
+                      '\n(Use the built-in keyboard at the bottom👇)',
+    'start_menu': 'You have entered the main menu!',
+    'photo_selection': 'I choose a photo for you!',
+    'developer': 'This bot was developed by Denis Kulikov. If you want to develop a Telegram bot for yourself, '
+                 'you can contact me by phone: +48789137022. Or by clicking on the button under this message, '
+                 'you can write me a private message in a telegram. I can develop a turnkey bot for you according '
+                 'to your wishes!',
+    'price list': '<u><b>Manicure:</b></u>\n\n'
+                  '<b>1.</b> Manicure + gel-polish - 20 zl\n'
+                  '<b>2.</b> Manicure + gel strengthening - 20 zl\n'
+                  '<b>3.</b> Manicure + strengthening with gel + gel-polish - 30 zl\n'
+                  '<b>4.</b> Manicure - 10 zl\n'
+                  '<b>5.</b> Removal of gel polish/acrylic (uncoated) - 10 zl\n'
+                  '<b>6.</b> Nail extensions - for free\n\n'
+                  '<u><b>Designs:</b></u>\n\n '
+                  '<b>1.</b> French, Ombre, Holes - free\n'
+                  '<b>2.</b> 1/10 nail design - free\n\n'
+                  '<u><b>Repair:</b></u>\n\n'
+                  '<b>1.</b>1/10 nail repair - free\n'
+                  '<b>2.</b> 1 nail extension - free'
+}
+
+lexicon_menu_en: dict[str, str] = {'/st': 'Start me from the beginning!',
+                                   '/he': 'Help on the work of the bot!'
+                                   }
+
+lexicon_button_en: dict[str, str] = {'button_1': 'My address',
+                                  'button_2': 'My contact phone number',
+                                  'button_3': 'Database of photos of my manicure works',
+                                  'button_4': 'Show a random photo of my manicure',
+                                  'button_5': 'Write to Iryna in Telegram',
+                                  'button_6': 'All information about the work of the wizard',
+                                  'button_7': 'Disinfection and sterilization of the instrument',
+                                  'button_8': 'Price list',
+                                  'button_9': 'My contact details',
+                                  'button_10': 'Go back to the main menu',
+                                  'button_100': 'Information about the bot developer',
+                                  'url_button_1': 'Write to Iryna in Telegram',
+                                  'url_button_2': 'Write to Denis in Telegram'
+                                  }
+
+lexicon_button_in_en: dict[str, str] = {'button_in_1': 'Further'}
+
+lexicon_disinfection: dict[str, str] = {
+    'phrase1': 'Everything you need for disinfection and sterilization of manicure tools!',
+    'phrase2': '<b>The disinfection and sterilization stages that my manicure tool goes through after each use:</b>'
+               '\n<b>1.</b> I pour the disinfectant <b>Medisept Viruton Forte</b> (one of the best products used in '
+               'medical institutions around the world to disinfect the instrument) into a special container.',
+    'phrase3': '<b>2.</b> I put the instrument in a disinfectant solution for 30 minutes.',
+    'phrase4': '<b>3.</b> I take out the tool after 30 minutes and wash it with soap solution.',
+    'phrase5': '<b>4.</b> After washing, the tool must be dried.',
+    'phrase6': '<b>5.</b> Further, for additional disinfection, I use a glasperlen (ball) sterilizer, which is '
+               'designed for disinfection treatment of manicure tools by immersing them in a flask with glass balls '
+               'heated to a temperature of 175 to 200 °C.',
+    'phrase7': '<b>6.</b> I insert the instrument into the sterilizer, in which it is disinfected for 1 minute.',
+    'phrase8': '<b>7.</b> After the sterilizer, I rinse the instrument again with a soap solution and dry it.',
+    'phrase9': 'This concludes the disinfection and sterilization procedure. <u><b>Our tool is clean, sterile '
+               'and ready for further use.</b></u>\pA you can find other information about my work using the '
+               'keyboard at the bottom of the screen👇'
+}
